701b9ddafaae90a59d81a735f5ad99bf22e07c8f
NonameNoname123 <zhenya.grigorieva116@yandex.ru>
2019-10-28 16:53:59 +0300

    feat: Signup screen views added. Dev in progress

--- app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signup/SignUpFragment.kt
+++ app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signup/SignUpFragment.kt
@@ -1,14 +1,41 @@
 package com.conceptic.andcourse.presentation.auth.signup
 
+import android.os.Bundle
+import android.view.View
 import com.conceptic.andcourse.R
 import com.conceptic.andcourse.presentation.base.BaseFragment
 import com.conceptic.andcourse.presentation.ext.createScope
+import com.conceptic.andcourse.usecase.auth.signup.Gender
+import kotlinx.android.synthetic.main.fragment_signup.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.scope.Scope
 
 class SignUpFragment : BaseFragment<SignUpViewModel>(R.layout.fragment_signup) {
     override val scope: Scope = createScope(SIGNUP_SCOPE)
     override val viewModel: SignUpViewModel by scope.viewModel(this)
+
+    private val email
+        get() = signup_input_email_value.text.toString()
+    private val dateBirth
+        get() = signup_input_date_birth_value.text.toString()
+    private val password
+        get() = signup_input_password_value.text.toString()
+    private val repeatPassword
+        get() = signup_input_rep_password_value.text.toString()
+    private val gender
+        get() = when (signup_radio_group_gender.checkedRadioButtonId) {
+            signup_radio_male.id -> Gender.MALE
+            signup_radio_female.id -> Gender.FEMALE
+            else -> throw IllegalArgumentException("Unhandled id")
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        signup_btn_accept.setOnClickListener {
+            viewModel.onAcceptBtnClicked(email, dateBirth, password, repeatPassword, gender)
+        }
+    }
 
     companion object {
         private const val SIGNUP_SCOPE = "signup_scope"
--- app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signup/SignUpViewModel.kt
+++ app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signup/SignUpViewModel.kt
@@ -1,10 +1,15 @@
 package com.conceptic.andcourse.presentation.auth.signup
 
 import com.conceptic.andcourse.presentation.base.BaseViewModel
+import com.conceptic.andcourse.usecase.auth.signup.Gender
 import com.conceptic.andcourse.usecase.auth.signup.SignUpCase
 
 class SignUpViewModel(
     private val signUpCase: SignUpCase
 ) : BaseViewModel() {
     override fun onStart() {}
+
+    fun onAcceptBtnClicked(email: String, dateBirth: String, password: String, repeatPassword: String, gender: Gender) {
+
+    }
 }
