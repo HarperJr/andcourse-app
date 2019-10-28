7b2d04eca201a6b7f3d6077bb050a65d29bd36f5
Nikita Sychev <ns@digipeople.ru>
2019-10-28 19:45:08 +0300

    ref: signin screen refactoring chores

--- app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signin/SignInFragment.kt
+++ app/src/main/kotlin/com/conceptic/andcourse/presentation/auth/signin/SignInFragment.kt
@@ -13,14 +13,16 @@ class SignInFragment : BaseFragment<SignInViewModel>(R.layout.fragment_signin) {
     override val scope: Scope = createScope(SIGNIN_SCOPE)
     override val viewModel: SignInViewModel by scope.viewModel(this)
 
-    private val email = { signin_input_email_value.text.toString() }
-    private val pass = { signin_input_pass_value.text.toString() }
+    private val email
+        get() = signin_input_email_value.text.toString()
+    private val pass
+        get() = signin_input_pass_value.text.toString()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         signin_btn_accept.setOnClickListener {
-            viewModel.onAcceptBtnClicked(email(), pass())
+            viewModel.onAcceptBtnClicked(email, pass)
         }
     }
 
